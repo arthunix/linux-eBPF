c10e46638f2e1a332b274af8a43f125c266bd9ff
arthur <thursilverio@gmail.com>
2022-08-10 09:42:55 -0300

    first commit

---
+++ config-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo Installing a basic C development enviroment
+sudo dnf group install "C Development Tools and Libraries" "Development Tools" -y
+sudo dnf install emacs clang llvm elfutils-libelf-devel -y
+sudo dnf search flex bison python3 minicom ncurses-devel make automake gcc gcc-c++ kernel-devel -y
+sudo dnf install qemu-system-x86 qemu-system-arm -y
+sudo dnf install kernel-devel kernel-headers kernel-doc -y
+
+echo Installing a bpf important packages and libraries
+sudo dnf install bcc bcc-devel bcc-tools libbpf libbpf-devel libbpf-tools -y
+sudo dnf install bpfmon bpftool bpftrace -y
