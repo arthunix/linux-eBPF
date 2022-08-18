be5bf0f0ecf1311a800fecc0a827018713fa24c3
Arthur Silverio <109439838+arthunix@users.noreply.github.com>
2022-08-18 18:48:40 -0300

    Update config-env.sh

--- config-env.sh
+++ config-env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo Installing a basic C development enviroment
+echo Installing a basic C and eBPF development enviroment
 sudo dnf group install "C Development Tools and Libraries" "Development Tools" -y
 sudo dnf install emacs clang llvm elfutils-libelf-devel -y
-sudo dnf search flex bison python3 minicom ncurses-devel make automake gcc gcc-c++ kernel-devel -y
+sudo dnf install flex bison python3 minicom ncurses-devel make automake gcc gcc-c++ kernel-devel -y
 sudo dnf install qemu-system-x86 qemu-system-arm -y
 sudo dnf install kernel-devel kernel-headers kernel-doc -y
 
