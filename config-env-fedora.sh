86b58072418f4fb058b7ee3d5cff076aaa67fb14
arthunix <thursilverio@gmail.com>
2022-10-26 09:52:04 -0300

    remove some useless dep

--- config-env-fedora.sh
+++ config-env-fedora.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 sudo dnf update --refresh
-echo ----- Installing a basic C and eBPF development enviroment -----
+
 sudo dnf group install "C Development Tools and Libraries" "Development Tools" -y
-sudo dnf install emacs clang llvm elfutils-libelf-devel -y
+sudo dnf install emacs clang llvm elfutils-libelf-devel elfutils-libelf glibc-devel -y
+sudo dnf install kernel-tools kernel-tools-libs kernel-tools-libs-devel kernel-core kernel-modules -y
+sudo dnf install binutils-devel binutils flex flex-devel bison bison-devel ncurses ncurses-devel ncurses-libs -y
 sudo dnf install flex bison python3 minicom ncurses-devel make automake gcc gcc-c++ kernel-devel -y
 sudo dnf install qemu-system-x86 qemu-system-arm kernel-devel kernel-headers kernel-doc -y
-
-echo ----- Installing a bpf important packages and libraries -----
 sudo dnf install bcc bcc-devel bcc-tools libbpf libbpf-devel libbpf-tools -y
-sudo dnf install bpfmon bpftool bpftrace -y
+sudo dnf install bpfmon bpftool bpftrace libpcap libpcap-devel -y
